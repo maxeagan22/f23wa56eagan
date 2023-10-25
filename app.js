16285af9f4f23738e54860c1d4d81fa2becbe504
maxeagan22 <124809445+maxeagan22@users.noreply.github.com>
2023-10-25 17:31:12 -0500

    new endpoint

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var myDataRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -15,6 +16,8 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use('/mydata', myDataRouter); //Added endpoint to myData
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
